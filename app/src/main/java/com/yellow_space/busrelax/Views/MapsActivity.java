9befa17b2f2369d7704290a0a213c781c38a563d
Vitalii Suslin <shakrek@gmail.com>
2017-10-21 14:19:47 +0300

    GPS tracker to find your personal location

--- app/src/main/java/com/yellow_space/busrelax/Views/MapsActivity.java
+++ app/src/main/java/com/yellow_space/busrelax/Views/MapsActivity.java
@@ -1,4 +1,4 @@
-package com.yellow_space.busrelax;
+package com.yellow_space.busrelax.Views;
 
 import android.Manifest;
 import android.hardware.Sensor;
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.yellow_space.busrelax.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
